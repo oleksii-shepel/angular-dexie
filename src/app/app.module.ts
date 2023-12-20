8b7ccce32c2bc6f59d8fb0c40bbe7a1428547c5b
Oleksii Shepel <alex.s@paymentop.com>
2023-12-20 16:44:49 +0200

    added runGenerator wrapper

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Action, InMemoryObjectState, MainModule, MiddlewareOperator, Reducer, StoreModule, combineReducers, createStore, supervisor } from 'dexie-state-syncer';
-import { EMPTY, Observable, defer, first, switchMap } from 'rxjs';
+import { EMPTY, Observable, Subject, concat, defer, first, switchMap } from 'rxjs';
 
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
@@ -27,20 +27,20 @@ export const thunkMiddleware = (): MiddlewareOperator<any> => {
   };
 }
 
-export const sagaMiddleware = <T>(saga: (action: T) => Generator<Promise<any>, void, any>): MiddlewareOperator<T> => {
+export const sagaMiddleware = <T>(saga: (action: T) => Generator<Promise<any>, void, any> | AsyncGenerator<Promise<any>, void, any>): MiddlewareOperator<T> => {
   return (source: Observable<T>) => (dispatch: Function, getState: Function) => {
     return new Observable(observer => {
       const subscription = source.subscribe({
-        next(action) {
+        async next(action) {
           const iterator = saga(action);
-          resolveIterator(iterator);
+          await resolveIterator(iterator);
         },
         error(err) { observer.error(err); },
         complete() { observer.complete(); }
       });
 
-      function resolveIterator(iterator: Iterator<Promise<any>>) {
-        const { value, done } = iterator.next();
+      async function resolveIterator(iterator: AsyncIterator<Promise<any>> | Iterator<Promise<any>>) {
+        const { value, done } = await Promise.resolve(iterator.next());
 
         if (!done) {
           value.then(result => {
@@ -58,6 +58,24 @@ export const sagaMiddleware = <T>(saga: (action: T) => Generator<Promise<any>, v
   };
 };
 
+
+function runGenerator<T>(generator: Generator<Promise<T>, T, T>): Promise<T> {
+  return new Promise((resolve, reject) => {
+    function step({value, done}: IteratorResult<Promise<T>, T>) {
+      const promise: Promise<T> = Promise.resolve(value);
+      if (done) {
+        resolve(promise);
+      } else {
+        promise.then(
+          result => step(generator.next(result)),
+          error => step(generator.throw(error))
+        );
+      }
+    }
+    step(generator.next());
+  });
+}
+
 // Logger middleware as an RxJS operator
 export const loggerMiddleware = <T>(): MiddlewareOperator<T> => (source: Observable<T>) => (dispatch: Function, getState: Function) =>
   new Observable<T>((observer) => {
@@ -71,6 +89,25 @@ export const loggerMiddleware = <T>(): MiddlewareOperator<T> => (source: Observa
       complete: () => observer.complete(),
     });
   });
+
+
+export function adaptMiddleware(middleware: any): (source: Observable<any>) => (dispatch: Function, getState: Function) => Observable<any> {
+  return (source: Observable<any>) => (dispatch: Function, getState: Function) => {
+    // Create a 'next' function that pushes actions into a Subject
+    const actionSubject = new Subject<any>();
+    const next = (action: any) => actionSubject.next(action);
+
+    // Call the Redux middleware with a fake 'store'
+    const middlewareOutput = middleware({ getState, dispatch })(next);
+
+    // Return an Observable that first runs the middleware, then emits actions from the Subject
+    return concat(
+      defer(() => middlewareOutput(source)),
+      actionSubject.asObservable()
+    );
+  };
+}
+
 
 function rootMetaReducer(reducer: Reducer<any>) {
   return function (state: any, action: Action<any>) {
@@ -98,7 +135,7 @@ const routes: Routes = [
     BrowserModule,
     RouterModule.forRoot(routes),
     StoreModule.forRoot({
-      transformers: [thunkMiddleware()],
+      transformers: [thunkMiddleware(), sagaMiddleware(function* rootSage() {})],
       processors: [loggerMiddleware()],
       reducers: {
       },
